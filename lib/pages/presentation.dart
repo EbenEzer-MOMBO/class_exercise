5ae80ab69524eaf976853e7875f152752e68ba92
EbenEzer-MOMBO <ebenezermombo@gmail.com>
2023-04-24 00:47:58 +0100

    Ajout carousel

--- lib/pages/presentation.dart
+++ lib/pages/presentation.dart
@@ -1,24 +1,42 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:class_exercise/component/my_card.dart';
 import 'package:flutter/material.dart';
 
 class Presentation extends StatelessWidget {
-  const Presentation({super.key});
+  Presentation({super.key});
+
+  final List<Widget> items = [
+    const MyCard(
+        nom: "MOMBO",
+        prenom: "Eben",
+        phone: "062646464",
+        photo: "assets/images/tumblr_4928ab9d86f0bd0c8bc15aa4668fb72b_74d38c36_500.jpg"
+    ),
+    const MyCard(
+        nom: "PLAYBOI",
+        prenom: "Carti",
+        phone: "062646464",
+        photo: "assets/images/playboi-carti-pfp-9.jpg"
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            MyCard(
-                nom: "MOMBO",
-                prenom: "Eben",
-                phone: "062646464",
-                photo: "lib/images/tumblr_4928ab9d86f0bd0c8bc15aa4668fb72b_74d38c36_500.jpg")
-          ],
-        ),
+        child: CarouselSlider(
+          items: items,
+          options: CarouselOptions(
+            height: 400,
+            autoPlay: true,
+            enlargeCenterPage: true,
+            aspectRatio: 16/9,
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enableInfiniteScroll: true,
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            viewportFraction: 0.8,
+          ),
+        )
       ),
     );
   }
